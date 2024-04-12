aefb9195f3bd8aae2a24b34f9e5d51dafe71631e
Jared Lambert <jaredclambert@gmail.com>
2024-04-12 11:18:31 -0400

    added some planets. added some zoom. added some todos

--- bodies.js
+++ bodies.js
@@ -1,4 +1,4 @@
-const SUN_RADIUS = 200;
+const SUN_RADIUS = 2000;
 const EARTH_RADIUS = 18.33;
 const MOON_RADIUS = 5;
 const MARS_RADIUS = 9.76;
@@ -6,24 +6,41 @@ const PHOBOS_RADIUS = 0.032;
 const DEIMOS_RADIUS = 0.02;
 const MERCURY_RADIUS = 7;
 const VENUS_RADIUS = 17;
-const JUPITER_RADIUS = 101;
-const IO_RADIUS = 10;
-const EUROPA_RADIUS = 10;
-const GANYMEDE_RADIUS = 10;
-const CALLISTO_RADIUS = 10;
+const JUPITER_RADIUS = 201;
+const IO_RADIUS = 5.25;
+const EUROPA_RADIUS = 4.49;
+const GANYMEDE_RADIUS = 7.57;
+const CALLISTO_RADIUS = 7.4;
+const SATURN_RADIUS = 167;
 
 const EARTH_DISTANCE = SUN_RADIUS + 150;
 const MOON_DISTANCE = EARTH_RADIUS + 50;
 const MARS_DISTANCE = SUN_RADIUS + 250;
 const PHOBOS_DISTANCE = MARS_RADIUS + 5;
 const DEIMOS_DISTANCE = MARS_RADIUS + 10;
-const MERCURY_DISTANCE = SUN_RADIUS + 10;
+const MERCURY_DISTANCE = SUN_RADIUS + 181944;
 const VENUS_DISTANCE = SUN_RADIUS + 50;
-const JUPITER_DISTANCE = SUN_RADIUS + 100;
+const JUPITER_DISTANCE = SUN_RADIUS + 700;
+const GANYMEDE_DISTANCE = JUPITER_RADIUS + 70;
 const IO_DISTANCE = JUPITER_RADIUS + 20;
-const EUROPA_DISTANCE = JUPITER_RADIUS + 30;
-const GANYMEDE_DISTANCE = JUPITER_RADIUS + 40;
-const CALLISTO_DISTANCE = JUPITER_RADIUS + 50;
+const EUROPA_DISTANCE = JUPITER_RADIUS + 40;
+const CALLISTO_DISTANCE = JUPITER_RADIUS + 200;
+const SATURN_DISTANCE = SUN_RADIUS + 1500;
+
+const SUN_PERIOD = 1;
+const MERCURY_PERIOD = 58.6 * 24 * 60 * 60;
+const VENUS_PERIOD = 243 * 24 * 60 * 60;
+const MARS_PERIOD = 687 * 24 * 60 * 60;
+const EARTH_PERIOD = 365.256 * 24 * 60 * 60;
+const MOON_PERIOD = 29.5 * 24 * 60 * 60;
+const JUPITER_PERIOD = 4333 * 24 * 60 * 60;
+const PHOBOS_PERIOD = 7.67 * 60 * 60;
+const DEIMOS_PERIOD = 30.25 * 60 * 60;
+const IO_PERIOD = 42.5 * 60 * 60;
+const EUROPA_PERIOD = 3.5 * 24 * 60 * 60;
+const GANYMEDE_PERIOD = 7.155 * 24 * 60 * 60;
+const CALLISTO_PERIOD = 16.689 * 24 * 60 * 60;
+const SATURN_PERIOD = 10756 * 24 * 60 * 60;
 
 class Body {
   static SUN = "sun";
@@ -41,8 +58,9 @@ export const bodyVars = {
     r: SUN_RADIUS,
     true_r: SUN_RADIUS,
     orbit: null,
-    period: 1,
+    period: SUN_PERIOD,
     distance: 0,
+    true_distance: 0,
   },
   mercury: {
     x: 0,
@@ -51,8 +69,9 @@ export const bodyVars = {
     r: MERCURY_RADIUS,
     true_r: MERCURY_RADIUS,
     orbit: Body.SUN,
-    period: 58.6 * 24 * 60 * 60,
+    period: MERCURY_PERIOD,
     distance: MERCURY_DISTANCE,
+    true_distance: MERCURY_DISTANCE,
   },
   venus: {
     x: 0,
@@ -61,8 +80,9 @@ export const bodyVars = {
     r: VENUS_RADIUS,
     true_r: VENUS_RADIUS,
     orbit: Body.SUN,
-    period: 243 * 24 * 60 * 60,
+    period: VENUS_PERIOD,
     distance: VENUS_DISTANCE,
+    true_distance: VENUS_DISTANCE,
   },
   earth: {
     x: 0,
@@ -71,8 +91,9 @@ export const bodyVars = {
     r: EARTH_RADIUS,
     true_r: EARTH_RADIUS,
     orbit: Body.SUN,
-    period: 365.256 * 24 * 60 * 60,
+    period: EARTH_PERIOD,
     distance: EARTH_DISTANCE,
+    true_distance: EARTH_DISTANCE,
   },
   moon: {
     x: 0,
@@ -81,8 +102,9 @@ export const bodyVars = {
     r: MOON_RADIUS,
     true_r: MOON_RADIUS,
     orbit: Body.EARTH,
-    period: 29.5 * 24 * 60 * 60,
+    period: MOON_PERIOD,
     distance: MOON_DISTANCE,
+    true_distance: MOON_DISTANCE,
   },
   mars: {
     x: 0,
@@ -91,8 +113,9 @@ export const bodyVars = {
     r: MARS_RADIUS,
     true_r: MARS_RADIUS,
     orbit: Body.SUN,
-    period: 687 * 24 * 60 * 60,
+    period: MARS_PERIOD,
     distance: MARS_DISTANCE,
+    true_distance: MARS_DISTANCE,
   },
   phobos: {
     x: 0,
@@ -101,8 +124,9 @@ export const bodyVars = {
     r: PHOBOS_RADIUS,
     true_r: PHOBOS_RADIUS,
     orbit: Body.MARS,
-    period: 7.67 * 60 * 60,
+    period: PHOBOS_PERIOD,
     distance: PHOBOS_DISTANCE,
+    true_distance: PHOBOS_DISTANCE,
   },
   deimos: {
     x: 0,
@@ -111,7 +135,78 @@ export const bodyVars = {
     r: DEIMOS_RADIUS,
     true_r: DEIMOS_RADIUS,
     orbit: Body.MARS,
-    period: 30.25 * 60 * 60,
+    period: DEIMOS_PERIOD,
     distance: DEIMOS_DISTANCE,
+    true_distance: DEIMOS_DISTANCE,
+  },
+  jupiter: {
+    x: 0,
+    y: 0,
+    z: 0,
+    r: JUPITER_RADIUS,
+    true_r: JUPITER_RADIUS,
+    orbit: Body.SUN,
+    period: JUPITER_PERIOD,
+    distance: JUPITER_DISTANCE,
+    true_distance: JUPITER_DISTANCE,
+  },
+  io: {
+    x: 0,
+    y: 0,
+    z: 0,
+    r: IO_RADIUS,
+    true_r: IO_RADIUS,
+    orbit: Body.JUPITER,
+    period: IO_PERIOD,
+    distance: IO_DISTANCE,
+    true_distance: IO_DISTANCE,
+  },
+  europa: {
+    x: 0,
+    y: 0,
+    z: 0,
+    r: EUROPA_RADIUS,
+    true_r: EUROPA_RADIUS,
+    orbit: Body.JUPITER,
+    period: EUROPA_PERIOD,
+    distance: EUROPA_DISTANCE,
+    true_distance: EUROPA_DISTANCE,
+  },
+  ganymede: {
+    x: 0,
+    y: 0,
+    z: 0,
+    r: GANYMEDE_RADIUS,
+    true_r: GANYMEDE_RADIUS,
+    orbit: Body.JUPITER,
+    period: GANYMEDE_PERIOD,
+    distance: GANYMEDE_DISTANCE,
+    true_distance: GANYMEDE_DISTANCE,
+  },
+  callisto: {
+    x: 0,
+    y: 0,
+    z: 0,
+    r: CALLISTO_RADIUS,
+    true_r: CALLISTO_RADIUS,
+    orbit: Body.JUPITER,
+    period: CALLISTO_PERIOD,
+    distance: CALLISTO_DISTANCE,
+    true_distance: CALLISTO_DISTANCE,
+  },
+  saturn: {
+    x: 0,
+    y: 0,
+    z: 0,
+    r: SATURN_RADIUS,
+    true_r: SATURN_RADIUS,
+    orbit: Body.SUN,
+    period: SATURN_PERIOD,
+    distance: SATURN_DISTANCE,
+    true_distance: SATURN_DISTANCE,
+    ring: {
+      inner_distance: SATURN_RADIUS + 100,
+      outer_distance: SATURN_RADIUS + 200,
+    },
   },
 };
--- globe.js
+++ globe.js
@@ -6,8 +6,13 @@ canvas.width = window.innerWidth;
 
 const ctx = canvas.getContext("2d");
 
+// constants
+// TODO: add an initial zoom level, change planet distances/sizes to generic ratios
+const UNIT_DISTANCE = 1000;
+// const UNIT_VIEW = Math.PI / 2;
 // variable params
-var obsDistance = 800;
+// var fieldOfView = Math.PI / 4;
+var obsDistance = UNIT_DISTANCE;
 var obsAngle = 0;
 var timeFactor = 60 * 60 * 5;
 var time = 0;
@@ -21,20 +26,39 @@ function drawGlobe(x, y, r) {
   ctx.fill();
 }
 
+function drawRing(innerRadius, outerRadius, x, y) {
+  ctx.beginPath();
+  ctx.lineWidth = outerRadius - innerRadius; // TODO: update this based on the angle of view. min 1, max outerRadius - innerRadius
+  ctx.arc(
+    canvas.width / 2 + x,
+    canvas.height / 2 + y,
+    (outerRadius + innerRadius) / 2,
+    0, // TODO: update this based on where it will intersect with the planet
+    2 * Math.PI // TODO: update this based on where it will intersect with the planet
+  );
+  ctx.stroke();
+}
+
 function orbitFactor(t, period, offset = 0) {
   return Math.cos((t / period) * 2 * Math.PI + offset);
 }
 
 function drawObjects() {
   let values = Object.values(bodyVars);
-  values.sort((a, b) => b["z"] - a["z"]);
+  values.sort((a, b) => b.z - a.z);
   for (let body of values) {
+    // if (body.z + obsDistance > 0) {}
     drawGlobe(body.x, body.y, body.r);
+    if (body.ring) {
+      drawRing(body.ring.inner_radius, body.ring.outer_radius, body.x, body.y);
+    }
   }
 }
 
 function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const canvasWidth = canvas.width;
+  const canvasHeight = canvas.height;
+  ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
   // draw celestial bodies
   drawObjects();
@@ -42,25 +66,38 @@ function draw() {
   // update parameters
   for (let key in bodyVars) {
     let body = bodyVars[key];
-    let primary = bodyVars[bodyVars[key].orbit];
+    let primary = bodyVars[body.orbit];
 
-    bodyVars[key].x = primary
+    body.distance = Math.max(
+      1,
+      (body.true_distance * (obsDistance - body.z) * UNIT_DISTANCE) /
+        (obsDistance * obsDistance)
+    );
+    body.x = primary
       ? primary.x + body.distance * orbitFactor(time, body.period)
       : body.x;
-    bodyVars[key].y =
+    body.y =
       (primary ? primary.y : 0) +
       Math.sin(obsAngle) *
         (primary
           ? body.distance * orbitFactor(time, body.period, Math.PI / 2)
           : body.y);
-    bodyVars[key].z =
+    body.z =
       (primary ? primary.z : 0) +
       Math.cos(obsAngle) *
         (primary
           ? body.distance * orbitFactor(time, body.period, Math.PI / 2)
           : body.z);
-    bodyVars[key].r =
-      Math.max(1, body.true_r * (obsDistance - body.z)) / obsDistance;
+    body.r = Math.max(
+      0,
+      (body.true_r * (obsDistance - body.z) * UNIT_DISTANCE) /
+        (obsDistance * obsDistance)
+    );
+    // body.r = calculateSize(
+    // body.true_r*2,
+    // Math.sqrt((obsDistance + body.z) ** 2 + body.x * body.x),
+    // canvasWidth
+    // ) / 2;
   }
 
   time += timeFactor;
@@ -69,12 +106,24 @@ function draw() {
   requestAnimationFrame(draw);
 }
 
+function calculateSize(diameter, distance, canvasWidth) {
+  const angle = 2 * Math.atan(diameter / (2 * distance));
+  return Math.max(1, (angle * canvasWidth) / fieldOfView);
+}
+
 function initListeners() {
   // increase obsAngle on scroll
   window.addEventListener("wheel", (e) => {
     //increase timefactor on shift + wheeel
     if (e.shiftKey) {
       timeFactor *= e.deltaY > 0 ? 1.02 : 1.0 / 1.02;
+    } else if (e.metaKey) {
+      obsDistance *= e.deltaY > 0 ? 1.02 : 1.0 / 1.02;
+      // field of view ranges from 0 to 180 degrees
+      // fieldOfView = Math.min(
+      // Math.PI,
+      // Math.max(0, (UNIT_VIEW * obsDistance) / UNIT_DISTANCE)
+      // );
     } else if (e.deltaY > 0) {
       obsAngle += 0.02;
       obsAngle = Math.min(0, obsAngle);
@@ -114,7 +163,7 @@ function initListeners() {
         obsAngle = Math.min(0, obsAngle);
       } else {
         obsAngle -= 0.02;
-        obsAngle = Math.min(0, Math.max(-Math.PI / 2, obsAngle));
+        obsAngle = Math.min(0, Math.max(-Math.PI, obsAngle));
       }
     },
     { passive: false }
